01465d77cff9e7b9c063a8bfe9fa3c1a38300132
wangyijieonline <wangyijieonline@126.com>
2017-08-22 16:54:48 +0800

    Echo调试成功

---
+++ Lwip/1.1tcpecho_client_Raw/user/ethernet/tcp_echoclient.c
@@ -0,0 +1,431 @@
+/**
+  ******************************************************************************
+  * @file    tcp_echoclient.c
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    31-July-2013 
+  * @brief   tcp echoclient application using LwIP RAW API
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "lwip/debug.h"
+#include "lwip/stats.h"
+#include "lwip/tcp.h"
+#include "main.h"
+#include "memp.h"
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+#if LWIP_TCP
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+u8_t  recev_buf[50];
+__IO uint32_t message_count=0;
+
+u8_t   data[100];
+
+struct tcp_pcb *echoclient_pcb;
+
+/* ECHO protocol states */
+enum echoclient_states
+{
+  ES_NOT_CONNECTED = 0,
+  ES_CONNECTED,
+  ES_RECEIVED,
+  ES_CLOSING,
+};
+
+
+/* structure to be passed as argument to the tcp callbacks */
+struct echoclient
+{
+  enum echoclient_states state; /* connection status */
+  struct tcp_pcb *pcb;          /* pointer on the current tcp_pcb */
+  struct pbuf *p_tx;            /* pointer on pbuf to be transmitted */
+};
+
+extern uint8_t flag_tcp_client;
+
+/* Private function prototypes -----------------------------------------------*/
+static err_t tcp_echoclient_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
+static void tcp_echoclient_connection_close(struct tcp_pcb *tpcb, struct echoclient * es);
+static err_t tcp_echoclient_poll(void *arg, struct tcp_pcb *tpcb);
+static err_t tcp_echoclient_sent(void *arg, struct tcp_pcb *tpcb, u16_t len);
+static void tcp_echoclient_send(struct tcp_pcb *tpcb, struct echoclient * es);
+static err_t tcp_echoclient_connected(void *arg, struct tcp_pcb *tpcb, err_t err);
+
+/* Private functions ---------------------------------------------------------*/
+
+
+/**
+* @brief  Connects to the TCP echo server
+* @param  None
+* @retval None
+*/
+void tcp_echoclient_connect(void)
+{
+  struct ip_addr DestIPaddr;
+	uint8_t iptxt[20];
+  
+  /* create new tcp pcb */
+  echoclient_pcb = tcp_new();
+  
+  if (echoclient_pcb != NULL)
+  {
+    IP4_ADDR( &DestIPaddr, DEST_IP_ADDR0, DEST_IP_ADDR1, DEST_IP_ADDR2, DEST_IP_ADDR3 );
+		
+		sprintf((char*)iptxt, " %d.%d.%d.%d", DEST_IP_ADDR0, DEST_IP_ADDR1, DEST_IP_ADDR2, DEST_IP_ADDR3);
+    
+    /* connect to destination address/port */
+    tcp_connect(echoclient_pcb,&DestIPaddr,DEST_PORT,tcp_echoclient_connected);
+		printf("Remote IPAddress: %s,Port: %d \r\n",iptxt,DEST_PORT);
+  }
+  else
+  {
+    /* deallocate the pcb */
+    memp_free(MEMP_TCP_PCB, echoclient_pcb);
+#ifdef SERIAL_DEBUG
+    printf("\n\r can not create tcp pcb");
+#endif 
+  }
+}
+
+/**
+  * @brief Function called when TCP connection established
+  * @param tpcb: pointer on the connection contol block
+  * @param err: when connection correctly established err should be ERR_OK 
+  * @retval err_t: returned error 
+  */
+static err_t tcp_echoclient_connected(void *arg, struct tcp_pcb *tpcb, err_t err)
+{
+  struct echoclient *es = NULL;
+  
+  if (err == ERR_OK)   
+  {
+    /* allocate structure es to maintain tcp connection informations */
+    es = (struct echoclient *)mem_malloc(sizeof(struct echoclient));
+  
+    if (es != NULL)
+    {
+      es->state = ES_CONNECTED;
+      es->pcb = tpcb;
+      
+      sprintf((char*)data, "sending tcp client message %d \r\n", message_count);
+        
+      /* allocate pbuf */
+      es->p_tx = pbuf_alloc(PBUF_TRANSPORT, strlen((char*)data) , PBUF_POOL);
+         
+      if (es->p_tx)
+      {       
+        /* copy data to pbuf */
+        pbuf_take(es->p_tx, (char*)data, strlen((char*)data));
+        
+        /* pass newly allocated es structure as argument to tpcb */
+        tcp_arg(tpcb, es);
+  
+        /* initialize LwIP tcp_recv callback function */ 
+        tcp_recv(tpcb, tcp_echoclient_recv);
+  
+        /* initialize LwIP tcp_sent callback function */
+        tcp_sent(tpcb, tcp_echoclient_sent);
+  
+        /* initialize LwIP tcp_poll callback function */
+        tcp_poll(tpcb, tcp_echoclient_poll, 1);
+    
+        /* send data */
+        tcp_echoclient_send(tpcb,es);
+        
+        return ERR_OK;
+      }
+    }
+    else
+    {
+      /* close connection */
+      tcp_echoclient_connection_close(tpcb, es);
+      
+      /* return memory allocation error */
+      return ERR_MEM;  
+    }
+  }
+  else
+  {
+    /* close connection */
+    tcp_echoclient_connection_close(tpcb, es);
+  }
+  return err;
+}
+    
+/**
+  * @brief tcp_receiv callback
+  * @param arg: argument to be passed to receive callback 
+  * @param tpcb: tcp connection control block 
+  * @param err: receive error code 
+  * @retval err_t: retuned error  
+  */
+//TCP Client接收数据缓冲区
+u8 tcp_client_recvbuf[1500];
+
+static err_t tcp_echoclient_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
+{ 
+  struct echoclient *es;
+	u32 data_len = 0;
+	struct pbuf *q;
+
+  err_t ret_err;
+  
+
+  LWIP_ASSERT("arg != NULL",arg != NULL);
+  
+  es = (struct echoclient *)arg;
+  
+  /* if we receive an empty tcp frame from server => close connection */
+  if (p == NULL)//如果收到空的数据帧
+  {
+    /* remote host closed connection */
+    es->state = ES_CLOSING;
+    if(es->p_tx == NULL)
+    {
+       /* we're done sending, close connection */
+       tcp_echoclient_connection_close(tpcb, es);
+    }
+    else
+    {    
+      /* send remaining data*/
+      tcp_echoclient_send(tpcb, es);
+    }
+    ret_err = ERR_OK;
+  }   
+  /* else : a non empty frame was received from echo server but for some reason err != ERR_OK */
+  else if(err != ERR_OK)//非空数据帧但是出现了某些原因
+  {
+    /* free received pbuf*/
+    pbuf_free(p);
+
+    ret_err = err;
+  }
+  else if(es->state == ES_CONNECTED)
+  {
+    /* increment message count */
+    message_count++;
+//		
+//		if(p!=NULL)//当处于连接状态并且接收到的数据不为空时
+//		{
+			memset(tcp_client_recvbuf,0,1500);  //数据接收缓冲区清零
+			for(q=p;q!=NULL;q=q->next)  //遍历完整个pbuf链表
+			{
+				//判断要拷贝到TCP_CLIENT_RX_BUFSIZE中的数据是否大于TCP_CLIENT_RX_BUFSIZE的剩余空间，如果大于
+				//的话就只拷贝TCP_CLIENT_RX_BUFSIZE中剩余长度的数据，否则的话就拷贝所有的数据
+				if(q->len > (1500-data_len)) memcpy(tcp_client_recvbuf+data_len,q->payload,(1500-data_len));//拷贝数据
+				else memcpy(tcp_client_recvbuf+data_len,q->payload,q->len);
+				data_len += q->len;  	
+				if(data_len > 1500) break; //超出TCP客户端接收数组,跳出	
+			}
+			if(flag_tcp_client&1<<3)//已开启Echo模式
+			{
+				flag_tcp_client|=(1<<2);
+//				printf("We Have Echo Some Message: %s \r\n",tcp_client_recvbuf);
+			}
+			else
+				printf("We Have Received Some Message: %s \r\n",tcp_client_recvbuf);
+ 			tcp_recved(tpcb,p->tot_len);//用于获取接收数据,通知LWIP可以获取更多数据
+			pbuf_free(p);  	//释放内存
+			ret_err=ERR_OK;
+//		}
+//         
+//    /* Acknowledge data reception */
+//    tcp_recved(tpcb, p->tot_len);  
+//    
+//    pbuf_free(p);
+//    tcp_echoclient_connection_close(tpcb, es);//关闭tcp连接
+    ret_err = ERR_OK;
+  }
+  /* data received when connection already closed */
+  else
+  {
+    /* Acknowledge data reception */
+    tcp_recved(tpcb, p->tot_len);
+    
+    /* free pbuf and do nothing */
+    pbuf_free(p);
+    ret_err = ERR_OK;
+  }
+  return ret_err;
+}
+
+/**
+  * @brief function used to send data
+  * @param  tpcb: tcp control block
+  * @param  es: pointer on structure of type echoclient containing info on data 
+  *             to be sent
+  * @retval None 
+  */
+static void tcp_echoclient_send(struct tcp_pcb *tpcb, struct echoclient * es)
+{
+  struct pbuf *ptr;
+  err_t wr_err = ERR_OK;
+ 
+  while ((wr_err == ERR_OK) &&
+         (es->p_tx != NULL) && 
+         (es->p_tx->len <= tcp_sndbuf(tpcb)))
+  {
+    
+    /* get pointer on pbuf from es structure */
+    ptr = es->p_tx;
+
+    /* enqueue data for transmission */
+    wr_err = tcp_write(tpcb, ptr->payload, ptr->len, 1);//把要发送的数据放到缓冲队列
+    
+    if (wr_err == ERR_OK)
+    { 
+      /* continue with next pbuf in chain (if any) */
+      es->p_tx = ptr->next;
+      
+      if(es->p_tx != NULL)
+      {
+        /* increment reference count for es->p */
+        pbuf_ref(es->p_tx);
+      }
+      
+      /* free pbuf: will free pbufs up to es->p (because es->p has a reference count > 0) */
+      pbuf_free(ptr);
+   }
+   else if(wr_err == ERR_MEM)
+   {
+      /* we are low on memory, try later, defer to poll */
+     es->p_tx = ptr;
+   }
+   else
+   {
+     /* other problem ?? */
+   }
+	 tcp_output(tpcb);		//将发送缓冲队列中的数据立即发送出去
+  }
+}
+
+/**
+  * @brief  This function implements the tcp_poll callback function
+  * @param  arg: pointer on argument passed to callback
+  * @param  tpcb: tcp connection control block
+  * @retval err_t: error code
+  */
+//TCP服务器发送数据内容
+char tcp_client_sendbuf[200]="Hello World";
+
+static err_t tcp_echoclient_poll(void *arg, struct tcp_pcb *tpcb)
+{
+  err_t ret_err;
+  struct echoclient *es;
+
+  es = (struct echoclient*)arg;
+  if (es != NULL)
+  {
+		if(flag_tcp_client&1<<2)//    if (es->p_tx != NULL)
+		{
+			es->p_tx=pbuf_alloc(PBUF_TRANSPORT, 20,PBUF_POOL);	//申请内存 
+			if(flag_tcp_client&1<<3)//已开启Echo模式
+			{
+				pbuf_take(es->p_tx,(char*)tcp_client_recvbuf,strlen((char*)tcp_client_recvbuf));	//将tcp_client_sentbuf[]中的数据拷贝到es->p_tx中
+				printf("We Have Echo Some Message: %s \r\n",tcp_client_recvbuf);
+			}
+			else
+			{
+				strcpy(tcp_client_sendbuf,"Hello World!");
+				printf("We Have Sent Some Message: %s \r\n",tcp_client_sendbuf);
+				pbuf_take(es->p_tx,(char*)tcp_client_sendbuf,20);	//将tcp_client_sentbuf[]中的数据拷贝到es->p_tx中
+			}
+
+      /* there is a remaining pbuf (chain) , try to send data */
+      tcp_echoclient_send(tpcb, es);
+			flag_tcp_client &=~(1<<2);
+		}else
+    {
+      /* no remaining pbuf (chain)  */
+      if(es->state == ES_CLOSING)
+      {
+        /* close tcp connection */
+        tcp_echoclient_connection_close(tpcb, es);
+      }
+    }
+    ret_err = ERR_OK;
+  }
+  else
+  {
+    /* nothing to be done */
+    tcp_abort(tpcb);
+    ret_err = ERR_ABRT;
+  }
+  return ret_err;
+}
+
+/**
+  * @brief  This function implements the tcp_sent LwIP callback (called when ACK
+  *         is received from remote host for sent data) 
+  * @param  arg: pointer on argument passed to callback
+  * @param  tcp_pcb: tcp connection control block
+  * @param  len: length of data sent 
+  * @retval err_t: returned error code
+  */
+static err_t tcp_echoclient_sent(void *arg, struct tcp_pcb *tpcb, u16_t len)
+{
+  struct echoclient *es;
+
+  LWIP_UNUSED_ARG(len);
+
+  es = (struct echoclient *)arg;
+  
+  if(es->p_tx != NULL)
+  {
+    /* still got pbufs to send */
+    tcp_echoclient_send(tpcb, es);
+  }
+
+  return ERR_OK;
+}
+
+/**
+  * @brief This function is used to close the tcp connection with server
+  * @param tpcb: tcp connection control block
+  * @param es: pointer on echoclient structure
+  * @retval None
+  */
+static void tcp_echoclient_connection_close(struct tcp_pcb *tpcb, struct echoclient * es )
+{
+  /* remove callbacks */
+  tcp_recv(tpcb, NULL);
+  tcp_sent(tpcb, NULL);
+  tcp_poll(tpcb, NULL,0);
+
+  if (es != NULL)
+  {
+    mem_free(es);
+  }
+
+  /* close tcp connection */
+  tcp_close(tpcb);
+}
+
+#endif /* LWIP_TCP */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
